101ed2f2bc343f8c3750b1b898678fd34b3245d8
csjhonathan <jhonathancarv.s@gmail.com>
2023-08-22 17:53:48 -0300

    feat: all medias crud has been implemented, except for some business rules on deletion that include uncreated entities

---
+++ src/media/entities/media.entity.ts
@@ -0,0 +1,11 @@
+export class Media {
+  public id: number;
+  public title: string;
+  public username: string;
+
+  constructor(id: number, title: string, username: string) {
+    this.id = id;
+    this.title = title;
+    this.username = username;
+  }
+}
---
+++ src/media/media.service.ts
@@ -0,0 +1,79 @@
+import {
+  ConflictException,
+  Injectable,
+  NotFoundException,
+} from '@nestjs/common';
+import { CreateMediaDto } from './dto/create-media.dto';
+import { UpdateMediaDto } from './dto/update-media.dto';
+import { Media } from './entities/media.entity';
+
+@Injectable()
+export class MediaService {
+  private medias: Media[];
+  private idCount: number;
+  constructor() {
+    this.medias = [];
+    this.idCount = this.medias.length + 1;
+  }
+
+  create(createMediaDto: CreateMediaDto) {
+    const { title, username } = createMediaDto;
+    const existsMedia = this.medias.some((media) => {
+      return media.title === title && media.username === username;
+    });
+    if (existsMedia) {
+      throw new ConflictException();
+    }
+    const id = this.idCount;
+    const media = new Media(id, title, username);
+    this.medias.push(media);
+    this.idCount = this.medias.length + 1;
+    return { id };
+  }
+
+  findAll() {
+    return this.medias;
+  }
+
+  findOne(id: number) {
+    const media = this.medias.find((media) => media.id === id);
+    if (!media) throw new NotFoundException();
+    return media;
+  }
+
+  update(id: number, updateMediaDto: UpdateMediaDto) {
+    const { title, username } = updateMediaDto;
+    const media = this.medias.find((media) => media.id === id);
+    if (!media) throw new NotFoundException();
+
+    const existsMedia = this.medias.some((media) => {
+      return (
+        media.title === title && media.username === username && media.id !== id
+      );
+    });
+
+    if (existsMedia) {
+      throw new ConflictException();
+    }
+
+    const index = media.id - 1;
+    const mediaToUpdate = this.medias[index];
+
+    mediaToUpdate.title = title;
+    mediaToUpdate.username = username;
+
+    return `This action updates a #${id} media`;
+  }
+
+  remove(id: number) {
+    const existsMedia = this.medias.some((media) => media.id === id);
+
+    if (!existsMedia) {
+      throw new NotFoundException();
+    }
+
+    this.medias = this.medias.filter((media) => media.id !== id);
+
+    return `This action removes a #${id} media`;
+  }
+}
